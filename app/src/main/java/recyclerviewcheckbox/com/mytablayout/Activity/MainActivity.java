8681312f9788cde0b86d2d9e436f40c26c42bd28
sakib <syed.sakib143@gmail.com>
2018-04-14 13:12:32 +0530

    First time commited....Sakib Syed

---
+++ app/src/main/java/recyclerviewcheckbox/com/mytablayout/Activity/MainActivity.java
@@ -0,0 +1,100 @@
+package recyclerviewcheckbox.com.mytablayout.Activity;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import recyclerviewcheckbox.com.mytablayout.Fragment.HomeFragment;
+import recyclerviewcheckbox.com.mytablayout.Fragment.MovieFragment;
+import recyclerviewcheckbox.com.mytablayout.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPagerAdapter viewPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+        getid();
+        setViewPager();
+    }
+
+    private void setViewPager() {
+        tabLayout.setupWithViewPager(viewPager);
+
+        viewPagerAdapter = new ViewPagerAdapter( getSupportFragmentManager() );
+        viewPagerAdapter.addFragment( new HomeFragment(), "Home" );
+        viewPagerAdapter.addFragment( new MovieFragment(), "Movies" );
+
+        viewPager.setAdapter( viewPagerAdapter );
+        viewPager.setOffscreenPageLimit( 2 );
+        viewPager.addOnPageChangeListener( new TabLayout.TabLayoutOnPageChangeListener( tabLayout ) );
+        tabLayout.addOnTabSelectedListener( new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem( tab.getPosition() );
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        } );
+    }
+
+
+    private void getid() {
+        try {
+            tabLayout = (TabLayout) findViewById( R.id.tabLayout );
+            viewPager = (ViewPager) findViewById( R.id.viewPager );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        ViewPagerAdapter(FragmentManager manager) {
+            super( manager );
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get( position );
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        void addFragment(Fragment fragment, String title) {
+            mFragmentList.add( fragment );
+            mFragmentTitleList.add( title );
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get( position );
+        }
+    }
+
+}
+
---
+++ app/src/main/java/recyclerviewcheckbox/com/mytablayout/Fragment/HomeFragment.java
@@ -0,0 +1,31 @@
+package recyclerviewcheckbox.com.mytablayout.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import recyclerviewcheckbox.com.mytablayout.R;
+
+/**
+ * Created by ombharti on 14-Apr-18.
+ */
+
+public class HomeFragment extends Fragment {
+
+    private View v;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Bundle args = getArguments();
+        v = inflater.inflate( R.layout.fragment_home, container, false);
+
+
+        return v;
+    }
+
+
+}
